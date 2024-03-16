509a384f917cc2ab8196bc6882803292ad5dcad4
Klyuchnikov_Yuriy <klyuchnikov_yuriy@mail.ru>
2024-03-16 21:54:20 +0300

    my three autotest

--- test_error_auth.py
+++ test_error_auth.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 LOGIN_BUTTON = (By.XPATH, '//*[@aria-label="Войти"]')
 EMAIL_FIELD = (By.XPATH, '//*[@type="email"]')
 NEXT_BUTTON = (By.XPATH, '//*[@id="identifierNext"]')
